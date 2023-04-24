b9e44fd5447356d5d24547427a9c3f48da50c5ba
“codewitherrors” <codewitherrors1@gmail.com>
2023-04-24 08:47:10 +0530

    Add : Find the Length of Line

---
+++ LineComparision/LineCompare.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComparision
+{
+    public class LineCompare
+    {
+        int x_One,x_Three,y_One,y_Two;
+        
+        //Constructor
+        public LineCompare(int x_One,int x_Two,int y_One,int y_Two) 
+        {
+            this.x_One = x_One;
+            x_Three = x_Two;
+            this.y_One = y_One;
+            this.y_Two = y_Two;
+        }
+
+        //Method
+        public void Calculate()
+        {
+            double length = Math.Sqrt(Math.Pow((x_Three-x_One),2)+Math.Pow((y_Two-y_One),2));
+            Console.WriteLine($"Length of the Line : {length}");
+        }
+    }
+}
---
+++ LineComparision/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Globalization;
+
+namespace LineComparision
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //Object Creation.
+            LineCompare obj = new LineCompare(20,30,40,50);
+            obj.Calculate();
+            Console.ReadKey();
+        }
+    }
+}
